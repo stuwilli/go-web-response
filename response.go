ad1c525d656b743d62d12da86b47fad8c6e185ba
Stuart Williams <stu.r.williams@gmail.com>
2017-03-15 21:21:47 +0000

    rework complete

--- response.go
+++ response.go
@@ -2,17 +2,69 @@ package webresponse
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 )
 
-//Response ...
+//Status ...
+type Status struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+}
+
+//Response  ....
 type Response struct {
-	Message    string      `json:"message"`
-	Timestamp  int64       `json:"timestamp"`
-	StatusCode int         `json:"status"`
-	Data       interface{} `json:"data,omitempty"`
-	Error      interface{} `json:"errors,omitempty"`
-	Path       string      `json:"path"`
+	Status
+	Timestamp int64             `json:"timestamp"`
+	Data      interface{}       `json:"data,omitempty"`
+	Errors    map[string]string `json:"errors,omitempty"`
+	Pageable  *Pageable         `json:"pageable,omitempty"`
+}
+
+//Pageable ....
+type Pageable struct {
+	Current int `json:"current,omitempty"`
+	Total   int `json:"total,omitempty"`
+	PerPage int `json:"perPage,omitempty"`
+}
+
+//NewResponse ....
+func NewResponse(statusCode int, data interface{}, err error) Response {
+
+	status := Status{StatusCode: statusCode, Message: http.StatusText(statusCode)}
+	resp := Response{Status: status, Timestamp: time.Now().Unix()}
+
+	if data != nil {
+		resp.Data = data
+	}
+
+	if err != nil {
+		resp.AddError(err)
+	}
+
+	return resp
+}
+
+//AddError ....
+func (r *Response) AddError(err error) {
+
+	if r.Errors == nil {
+		r.Errors = make(map[string]string)
+	}
+
+	count := len(r.Errors) + 1
+	r.Errors[fmt.Sprintf("error_%v", count)] = err.Error()
+}
+
+//AddNamedError ...
+func (r *Response) AddNamedError(name string, err error) {
+
+	if r.Errors == nil {
+		r.Errors = make(map[string]string)
+	}
+
+	r.Errors[name] = err.Error()
 }
 
 //MarshalJSON ...
--- response_test.go
+++ response_test.go
@@ -1,32 +1,88 @@
-package webresponse_test
+package webresponse
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/stuwilli/go-test-utils"
-	wr "github.com/stuwilli/go-web-response"
 )
 
-var testResponse wr.Response
+func TestNewResponse_WithData(t *testing.T) {
 
-func init() {
+	data := map[string]string{
+		"Test1": "This is test one",
+		"Test2": "This is test two",
+	}
 
-	testResponse = wr.Response{Message: "Test",
-		StatusCode: 200,
-		Timestamp:  1,
+	r := NewResponse(200, data, nil)
+
+	if len(r.Data.(map[string]string)) != 2 {
+		t.Error("Expected 2 data attributes")
+	}
+
+	if r.Message != "OK" {
+		t.Error("Expected message to be 'OK', got", r.Message)
+	}
+}
+
+func TestNewResponse_WithError(t *testing.T) {
+
+	r := NewResponse(http.StatusBadRequest, nil, errors.New("Test"))
+
+	if len(r.Errors) != 1 {
+		t.Error("Expected there to be 1 error")
+	}
+}
+
+//TestAddError ...
+func TestAddError(t *testing.T) {
+
+	r := NewResponse(200, nil, nil)
+
+	r.AddError(errors.New("Test"))
+
+	if r.Errors["error_1"] != "Test" {
+		t.Error("Expected error_1 to be set and to equal Test")
+	}
+
+	r.AddError(errors.New("Test2"))
+
+	if len(r.Errors) != 2 {
+		t.Error("Expected error count to be 2, got", len(r.Errors))
+	}
+
+	if r.Errors["error_2"] != "Test2" {
+		t.Error("Expected error_2 to be set and to equal Test2")
+	}
+}
+
+func TestAddNamedError(t *testing.T) {
+
+	r := NewResponse(200, nil, nil)
+
+	r.AddNamedError("test", errors.New("Test"))
+
+	if len(r.Errors) != 1 {
+		t.Error("Expected one error")
+	}
+
+	if r.Errors["test"] != "Test" {
+		t.Error("Expected error with key 'test' and value 'Test'")
 	}
 }
 
 func TestWriteJSON(t *testing.T) {
 
-	data := make(map[string]string)
-	data["test1"] = "foo"
-	data["test2"] = "bar"
+	data := map[string]string{
+		"Test1": "This is test one",
+		"Test2": "This is test two",
+	}
 
-	testResponse.Data = data
+	r := NewResponse(200, data, errors.New("Test"))
+	r.Timestamp = 1
 	w := fakeresponse.NewFakeResponse(t)
-	testResponse.WriteJSON(w)
-
-	expected := `{"message":"Test","timestamp":1,"status":200,"data":{"test1":"foo","test2":"bar"},"path":""}`
-	w.Assert(200, expected)
+	r.WriteJSON(w)
+	equal := `{"statusCode":200,"message":"OK","timestamp":1,"data":{"Test1":"This is test one","Test2":"This is test two"},"errors":{"error_1":"Test"}}`
+	w.Assert(200, equal)
 }
