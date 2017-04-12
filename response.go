ffdbf8a692d66dbb35561abb335c84899176e272
Stuart Williams <stu.r.williams@gmail.com>
2017-04-12 22:05:32 +0100

    Reworked using builder

--- response.go
+++ response.go
@@ -1,3 +1,4 @@
+//Package webresponse provides a builder for creating JSON and XML web responses
 package webresponse
 
 import (
@@ -7,67 +8,43 @@ import (
 	"time"
 )
 
-//Status ...
+//Status holds the status code and message
 type Status struct {
 	StatusCode int    `json:"statusCode"`
 	Message    string `json:"message"`
 }
 
-//Response  ....
+//ResponseBuilder interface provides the builder methods
+type ResponseBuilder interface {
+	Status(int) ResponseBuilder
+	Data(interface{}) ResponseBuilder
+	Errors(interface{}) ResponseBuilder
+	Error(error) ResponseBuilder
+	NamedError(string, string) ResponseBuilder
+	Build() Response
+}
+
+//Response holds the reponse data
 type Response struct {
 	Status
-	Timestamp int64             `json:"timestamp"`
-	Data      interface{}       `json:"data,omitempty"`
-	Errors    map[string]string `json:"errors,omitempty"`
-	Pageable  *Pageable         `json:"pageable,omitempty"`
+	Timestamp int64       `json:"timestamp"`
+	Data      interface{} `json:"data,omitempty"`
+	Errors    interface{} `json:"errors,omitempty"`
 }
 
-//Pageable ....
-type Pageable struct {
-	Current int `json:"current,omitempty"`
-	Total   int `json:"total,omitempty"`
-	PerPage int `json:"perPage,omitempty"`
+//responseBuilder impliments ResponseBuilder
+type responseBuilder struct {
+	status int
+	data   interface{}
+	errors interface{}
 }
 
-//NewResponse ....
-func NewResponse(statusCode int, data interface{}, err error) Response {
-
-	status := Status{StatusCode: statusCode, Message: http.StatusText(statusCode)}
-	resp := Response{Status: status, Timestamp: time.Now().Unix()}
-
-	if data != nil {
-		resp.Data = data
-	}
-
-	if err != nil {
-		resp.AddError(err)
-	}
-
-	return resp
+//NewBuilder creates a new ResponseBuilder, the status code defaults to 200
+func NewBuilder() ResponseBuilder {
+	return &responseBuilder{status: 200}
 }
 
-//AddError ....
-func (r *Response) AddError(err error) {
-
-	if r.Errors == nil {
-		r.Errors = make(map[string]string)
-	}
-
-	count := len(r.Errors) + 1
-	r.Errors[fmt.Sprintf("error_%v", count)] = err.Error()
-}
-
-//AddNamedError ...
-func (r *Response) AddNamedError(name string, err error) {
-
-	if r.Errors == nil {
-		r.Errors = make(map[string]string)
-	}
-
-	r.Errors[name] = err.Error()
-}
-
-//MarshalJSON ...
+//MarshalJSON marshalls the Response to JSON and returns a byte array
 func (r *Response) marshalJSON() []byte {
 
 	out, _ := json.Marshal(r)
@@ -75,10 +52,96 @@ func (r *Response) marshalJSON() []byte {
 	return out
 }
 
-//WriteJSON ...
+//WriteJSON writes the JSON response to a response writer w
 func (r *Response) WriteJSON(w http.ResponseWriter) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(r.StatusCode)
 	w.Write(r.marshalJSON())
+}
+
+//Data adds a data item to the reponse can be anything and returns the ResponseBuilder
+func (r *responseBuilder) Data(d interface{}) ResponseBuilder {
+	r.data = d
+	return r
+}
+
+//Status sets the status code for the response and return the ResponseBuilder
+func (r *responseBuilder) Status(s int) ResponseBuilder {
+	r.status = s
+	return r
+}
+
+//Errors adds a custom error type or a single error and returns ResponseBuilder
+func (r *responseBuilder) Errors(e interface{}) ResponseBuilder {
+
+	switch e.(type) {
+
+	case error:
+		r.Error(e.(error))
+
+	default:
+		r.errors = e
+	}
+
+	return r
+}
+
+//checkAndInitErrors checks to see if errors has been initialised
+//if not it initialises a map[string]string and returns the index +1
+//if already initialised as a map[string]string it return the current index + 1
+func (r *responseBuilder) checkAndInitErrors() int {
+
+	_, ok := r.errors.(map[string]string)
+
+	if r.errors == nil || !ok {
+		r.errors = make(map[string]string)
+	}
+
+	return len(r.errors.(map[string]string)) + 1
+}
+
+//Error takes an error and adds the message to a map[string]string
+//returns ResponseBuilder
+func (r *responseBuilder) Error(e error) ResponseBuilder {
+
+	count := r.checkAndInitErrors()
+
+	r.NamedError(fmt.Sprintf("error_%v", count), e.Error())
+
+	return r
+}
+
+//NamedError add a named error to the error map returns the ResponseBuilder
+func (r *responseBuilder) NamedError(k string, v string) ResponseBuilder {
+
+	r.checkAndInitErrors()
+
+	r.errors.(map[string]string)[k] = v
+
+	return r
+}
+
+//Build creates and return the Response
+func (r *responseBuilder) Build() Response {
+
+	status := Status{StatusCode: r.status, Message: http.StatusText(r.status)}
+	return Response{Status: status, Data: r.data, Errors: r.errors,
+		Timestamp: time.Now().Unix()}
+}
+
+//NewResponse legacy response building method
+func NewResponse(status int, data interface{}, err error) Response {
+
+	b := NewBuilder().Status(status)
+
+	if data != nil {
+		b.Data(data)
+	}
+
+	if err != nil {
+		b.Error(err)
+	}
+
+	return b.Build()
 }
--- response_test.go
+++ response_test.go
@@ -2,87 +2,130 @@ package webresponse
 
 import (
 	"errors"
-	"net/http"
 	"testing"
 
 	"github.com/stuwilli/go-test-utils"
 )
 
-func TestNewResponse_WithData(t *testing.T) {
+func TestResponseBuilder_WithDataStruct(t *testing.T) {
 
-	data := map[string]string{
-		"Test1": "This is test one",
-		"Test2": "This is test two",
+	type Test struct {
+		Prop string
 	}
+
+	x := Test{Prop: "Hi"}
+
+	b := NewBuilder()
+
+	r := b.Status(200).Data(x).Build()
+
+	r.Timestamp = 1
+
+	if r.Data.(Test).Prop != "Hi" {
+		t.Error("Expected hi")
+	}
+
+	expected := `{"statusCode":200,"message":"OK","timestamp":1,"data":{"Prop":"Hi"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(200, expected)
+
+}
+
+func TestResponseBuilder_WithRealError(t *testing.T) {
+
+	b := NewBuilder()
+	e1 := errors.New("An error")
+	e2 := errors.New("Another error")
+	r := b.Status(500).Error(e1).Error(e2).Build()
+	r.Timestamp = 1
+	//fmt.Println(string(r.marshalJSON()))
+
+	expected := `{"statusCode":500,"message":"Internal Server Error","timestamp":1,"errors":{"error_1":"An error","error_2":"Another error"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(500, expected)
+
+}
+
+func TestResponseBuilder_WithNamedError(t *testing.T) {
+
+	b := NewBuilder()
+	r := b.Status(500).NamedError("error1", "An error").NamedError("error2", "Another error").Build()
+	r.Timestamp = 1
+
+	expected := `{"statusCode":500,"message":"Internal Server Error","timestamp":1,"errors":{"error1":"An error","error2":"Another error"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(500, expected)
+}
+
+func TestResponseBuilder_WithErrorsMap(t *testing.T) {
+	err := map[string]string{
+		"error1": "An error",
+		"error2": "Another error",
+	}
+
+	b := NewBuilder()
+	r := b.Status(500).Errors(err).Build()
+	r.Timestamp = 1
+
+	expected := `{"statusCode":500,"message":"Internal Server Error","timestamp":1,"errors":{"error1":"An error","error2":"Another error"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(500, expected)
+}
+
+func TestResponseBuilder_ErrorsWithError(t *testing.T) {
+
+	b := NewBuilder()
+	r := b.Status(500).Errors(errors.New("An error")).Build()
+	r.Timestamp = 1
+
+	expected := `{"statusCode":500,"message":"Internal Server Error","timestamp":1,"errors":{"error_1":"An error"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(500, expected)
+}
+
+func TestResponseBuilder_NewResponseData(t *testing.T) {
+
+	type Test struct {
+		Prop string
+	}
+
+	data := Test{Prop: "Hi"}
 
 	r := NewResponse(200, data, nil)
-
-	if len(r.Data.(map[string]string)) != 2 {
-		t.Error("Expected 2 data attributes")
-	}
-
-	if r.Message != "OK" {
-		t.Error("Expected message to be 'OK', got", r.Message)
-	}
-}
-
-func TestNewResponse_WithError(t *testing.T) {
-
-	r := NewResponse(http.StatusBadRequest, nil, errors.New("Test"))
-
-	if len(r.Errors) != 1 {
-		t.Error("Expected there to be 1 error")
-	}
-}
-
-//TestAddError ...
-func TestAddError(t *testing.T) {
-
-	r := NewResponse(200, nil, nil)
-
-	r.AddError(errors.New("Test"))
-
-	if r.Errors["error_1"] != "Test" {
-		t.Error("Expected error_1 to be set and to equal Test")
-	}
-
-	r.AddError(errors.New("Test2"))
-
-	if len(r.Errors) != 2 {
-		t.Error("Expected error count to be 2, got", len(r.Errors))
-	}
-
-	if r.Errors["error_2"] != "Test2" {
-		t.Error("Expected error_2 to be set and to equal Test2")
-	}
-}
-
-func TestAddNamedError(t *testing.T) {
-
-	r := NewResponse(200, nil, nil)
-
-	r.AddNamedError("test", errors.New("Test"))
-
-	if len(r.Errors) != 1 {
-		t.Error("Expected one error")
-	}
-
-	if r.Errors["test"] != "Test" {
-		t.Error("Expected error with key 'test' and value 'Test'")
-	}
-}
-
-func TestWriteJSON(t *testing.T) {
-
-	data := map[string]string{
-		"Test1": "This is test one",
-		"Test2": "This is test two",
-	}
-
-	r := NewResponse(200, data, errors.New("Test"))
 	r.Timestamp = 1
+
+	expected := `{"statusCode":200,"message":"OK","timestamp":1,"data":{"Prop":"Hi"}}`
+
 	w := fakeresponse.NewFakeResponse(t)
+
 	r.WriteJSON(w)
-	equal := `{"statusCode":200,"message":"OK","timestamp":1,"data":{"Test1":"This is test one","Test2":"This is test two"},"errors":{"error_1":"Test"}}`
-	w.Assert(200, equal)
+	w.Assert(200, expected)
+}
+
+func TestResponseBuilder_NewResponseError(t *testing.T) {
+
+	r := NewResponse(500, nil, errors.New("An error"))
+	r.Timestamp = 1
+
+	expected := `{"statusCode":500,"message":"Internal Server Error","timestamp":1,"errors":{"error_1":"An error"}}`
+
+	w := fakeresponse.NewFakeResponse(t)
+
+	r.WriteJSON(w)
+	w.Assert(500, expected)
 }
